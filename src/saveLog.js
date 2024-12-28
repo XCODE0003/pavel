ac7e26211e1c089423d244b49773742f9e068eaa
KASRO1 <nikita150489@mail.ru>
2024-12-28 16:16:43 +0500

    ss

--- src/saveLog.js
+++ src/saveLog.js
@@ -85,9 +85,9 @@ ${log.bot.includes(':') ? `ℹ️ Добытый с бота @${(await new Teleg
 
             const error = (response.data?.errors || response.data?.error)?.[0] || (((response?.status || 200) === 200 || response?.status === 502) ? null : 'Технические работы LZT маркета');
             if (!error) {
-                console.log(response.data)
 
                 const account = response?.data?.item?.items?.[0]
+                console.log(account)
                 const item_id = account?.item_id
 
                 msg += `\n\n*✅ Успешно выложен на лолз LZT(https://lzt.market/${item_id})*`
